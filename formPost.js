ebfacb3650f5d04927b7e4991db250bab36c3900
Mahdi Khavari <mkhavari813@gmail.com>
2021-06-05 20:59:41 +0430

    add Post module

--- formPost.js
+++ formPost.js
@@ -1,18 +1,43 @@
-const url = 'https://607e443702a23c0017e8b097.mockapi.io/database'
+const url = 'https://c8c8ad1a-8be3-42cb-a6f4-8f2451733e59.mock.pstmn.io/recData'
 
-const data = { username: 'example' };
+const data = {}
 
-fetch(url, {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
+// fetch(url, {
+//   method: 'POST', // or 'PUT'
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(data),
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Success:', data);
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+
+const inputModals = document.querySelectorAll('.modalInput')
+const sendModal = document.querySelector('.sendModal')
+const fileInput = document.querySelector('.fileInput')
+// console.log(inputModals[3])
+
+sendModal.addEventListener('click',function(){
+    inputModals.forEach((item)=>{
+        data[item.id] = item.value
+    })
+    // data[fileInput.id] = showPreviewOne()
+    console.log(data)
 })
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
+
+function showPreviewOne(){
+    
+}
+
+fileInput.addEventListener('change',function(event){
+    let src = URL.createObjectURL(event.target.files[0]);
+    data[fileInput.id] = src
 })
-.catch((error) => {
-  console.error('Error:', error);
-});
+
+// fileInputSrc = showPreviewOne()
