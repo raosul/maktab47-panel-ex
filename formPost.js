0d3e22d2b47bdd77df28290155424873ed4f5060
rasoul <rasoulkordestani12@gmail.com>
2021-06-06 08:48:21 +0430

    install babel in order to use import export without error

--- formPost.js
+++ formPost.js
@@ -1,4 +1,5 @@
-const url = 'http://localhost:3000/products'
+import formGET from "./formGET";
+const url = 'http://localhost:3000/products';
 
 const data = {}
 
@@ -10,35 +11,35 @@ const sendModal = document.querySelector('.sendModal')
 const fileInput = document.querySelector('.fileInput')
 // console.log(inputModals[3])
 
-sendModal.addEventListener('click',function(){
-    inputModals.forEach((item)=>{
-        data[item.id] = item.value
+sendModal.addEventListener('click', function () {
+  inputModals.forEach((item) => {
+    data[item.id] = item.value
+  })
+  // data[fileInput.id] = showPreviewOne()
+  console.log(data)
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
     })
-    // data[fileInput.id] = showPreviewOne()
-    console.log(data)
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+    .catch((error) => {
+      console.error('Error:', error);
     });
 })
 
 // function showPreviewOne(){
-    
+
 // }
 
-fileInput.addEventListener('change',function(event){
-    let src = URL.createObjectURL(event.target.files[0]);
-    data[fileInput.id] = src
+fileInput.addEventListener('change', function (event) {
+  let src = URL.createObjectURL(event.target.files[0]);
+  data[fileInput.id] = src
 })
 
 // fileInputSrc = showPreviewOne()
